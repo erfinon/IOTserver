25933bcb3fc1c60b6e45029454618117ad2c2b20
erfinon <seyederfanjamali@seyeds-MacBook-Pro.local>
2023-04-04 19:34:37 +0330

    C

--- main.js
+++ main.js
@@ -1,6 +1,83 @@
-var http = require('http');
+'use strict'
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(3000);
+/**
+ * Module dependencies.
+ */
+
+var express = require('../../');
+
+const secret = "erfi";
+const repo = "~/home/www/IOTserver";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+
+
+var app = module.exports = express();
+
+// create an error with .status. we
+// can then use the property in our
+// custom error handler (Connect respects this prop as well)
+
+function error(status, msg) {
+  var err = new Error(msg);
+  err.status = status;
+  return err;
+}
+
+// if we wanted to supply more than JSON, we could
+// use something similar to the content-negotiation
+// example.
+
+// here we validate the API key,
+// by mounting this middleware to /api
+// meaning only paths prefixed with "/api"
+// will cause this middleware to be invoked
+
+app.use('/api', function(req, res, next){
+  var key = req.query['api-key'];
+
+  // key isn't present
+  if (!key) return next(error(400, 'api key required'));
+
+  // key is invalid
+  if (apiKeys.indexOf(key) === -1) return next(error(401, 'invalid api key'))
+
+  // all good, store req.key for route access
+  req.key = key;
+  next();
+});
+
+
+// example: http://localhost:3000/api/users/?api-key=foo
+app.get('/api/users', function (req, res) {
+    res.send("AAAA");
+  });
+
+// middleware with an arity of 4 are considered
+// error handling middleware. When you next(err)
+// it will be passed through the defined middleware
+// in order, but ONLY those with an arity of 4, ignoring
+// regular middleware.
+app.use(function(err, req, res, next){
+  // whatever you want here, feel free to populate
+  // properties on `err` to treat it differently in here.
+  res.status(err.status || 500);
+  res.send({ error: err.message });
+});
+
+// our custom JSON 404 middleware. Since it's placed last
+// it will be the last middleware called, if all others
+// invoke next() and do not respond.
+app.use(function(req, res){
+  res.status(404);
+  res.send({ error: "Sorry, can't find that" })
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
--- webhook.js
+++ webhook.js
@@ -1,6 +1,18 @@
-var http = require('http');
+const secret = "erfi";
+const repo = "~/home/www/IOTserver";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(3333);
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            exec('cd ' + repo + ' && git pull');
+        }
+    });
+
+    res.end();
+}).listen(8080);
