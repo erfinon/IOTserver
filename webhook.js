aed27b6cec1335069812072bfefeb9c717543bf5
erfak <erfinon@gmail.com>
2023-04-08 18:43:27 +0330

    c

--- webhook.js
+++ webhook.js
@@ -1,4 +1,4 @@
-const secret = "erfi";
+const secret = "your_secret_here";
 const repo = "~/home/www/IOTserver";
 
 const http = require('http');
@@ -6,13 +6,7 @@ const crypto = require('crypto');
 const exec = require('child_process').exec;
 
 http.createServer(function (req, res) {
-    req.on('data', function(chunk) {
-        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
-
-        if (req.headers['x-hub-signature'] == sig) {
-            exec('cd ' + repo + ' && git pull');
-        }
+    exec('cd ' + repo + ' && git pull');
     });
-
     res.end();
 }).listen(8080);
